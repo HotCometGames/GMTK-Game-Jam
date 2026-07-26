808852cb3022156aa5f0f4c579cfff7265313669
kl1226 <kianl26@mit.edu>
2026-07-25 22:05:30 -0400

    Level 3k and 2k updates
    new levels

--- Assets/Scripts/Actions/JumpAction.cs
+++ Assets/Scripts/Actions/JumpAction.cs
@@ -6,6 +6,19 @@ public class JumpAction : MoveActionBase
     [Header("Jump Settings")]
     [SerializeField] private float jumpForce = 12f;
 
+    private static bool hasAttemptedLevel2K;
+    private bool reduceFirst2KJump;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void ResetAttemptState() => hasAttemptedLevel2K = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        reduceFirst2KJump = gameObject.scene.name == "Level 2K" && !hasAttemptedLevel2K;
+        hasAttemptedLevel2K |= reduceFirst2KJump;
+    }
+
     protected override bool CanExecute()
     {
         return movement.IsGrounded;
@@ -13,7 +26,8 @@ public class JumpAction : MoveActionBase
 
     protected override void Execute()
     {
-        movement.SetVerticalVelocity(jumpForce);
+        movement.SetVerticalVelocity(jumpForce * (reduceFirst2KJump ? 0.94f : 1f));
+        reduceFirst2KJump = false;
     }
 
     // Push off downward: the dust is what the player kicked away to get airborne, so it should
